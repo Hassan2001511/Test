985f295055543973fc00d7f59469147ce2ca783d
Hassan2001511 <Hassan.2001511@gmail.com>
2017-04-04 09:50:47 -0400

    Add files via upload

---
+++ trick.py
@@ -0,0 +1,23 @@
+#1
+a="Programming"
+print("Reverse is",a[::-1])
+
+#2
+a=[4,5,6]
+x,y,z=a
+print(x)
+print(y)
+print(z)
+
+
+#3
+a=["Hassan","Bessam","AL-Jewad"]
+print(" ".join(a))
+
+#4
+a=5
+b=7
+a, b=b, a
+print(a)
+print(b)
+
